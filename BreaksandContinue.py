9334ea53808196adc82ba9a73539804345d5e8b0
Nicolas <nicjcoding@gmail.com>
2023-10-22 20:59:35 -0500

    Breaks and Continue

---
+++ BreaksandContinue.py
@@ -0,0 +1,22 @@
+# Breaks
+
+"""
+color = ["Black", "White", "Orange", "yellow"]
+
+for i in color:
+    print(i)
+    if i == "White":
+        print(True)
+        break
+
+string = ""
+while True:
+    user_input = input("Enter characters: ")
+    if user_input == "quit":
+        print(string)
+        break
+
+string = string + user_input
+    """
+
+# Continue Statement
