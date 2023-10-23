a6aa4ad8b0dab4d03a4825de3ecde006078432eb
Nicolas <nicjcoding@gmail.com>
2023-10-22 23:05:55 -0500

    Finished Loop Control

--- BreaksandContinue.py
+++ BreaksandContinue.py
@@ -20,3 +20,26 @@ string = string + user_input
     """
 
 # Continue Statement
+""" 
+for i in range(1,11):
+    if i % 3 == 0:
+        continue
+
+    print(i)
+
+
+counter = 1
+while counter <= 10:
+    
+    if counter % 2 == 0:
+
+        counter += 1
+        continue
+    print(counter)
+    counter += 1
+"""
+
+# Pass Statement - still goes through whether true or false - placeholder for the future code.
+
+for i in range(1,11):
+    pass
